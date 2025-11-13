521b743eea413825f76d819e44606afd0a463a7e
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2025-11-13 09:17:34 +0100

    ci: add minimalistic style check

--- flake.nix
+++ flake.nix
@@ -7,13 +7,24 @@
     nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
   };
 
-  outputs = inputs@{ self, ... }: {
-    nixosModules = import ./modules;
+  outputs =
+    inputs@{ self, ... }:
+    {
+      nixosModules = import ./modules;
 
-    # For now, tests run only on x86_64. Will expand a bit later.
-    checks.x86_64-linux = import ./checks {
-      pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-      modules = self.nixosModules;
+      # For now, tests run only on x86_64. Will expand a bit later.
+      checks.x86_64-linux = import ./checks {
+        pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+        modules = self.nixosModules;
+      };
+
+      # Common developer tooling. We also use this in the CI.
+      devShells.x86_64-linux.default =
+        let
+          pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+        in
+        pkgs.mkShell {
+          packages = [ pkgs.nixpkgs-fmt ];
+        };
     };
-  };
 }
