2e94cc585cc3868b012b2c0b0885a536bd5b320d
Martin Messer <martin.messer@cyberus-technology.de>
2025-11-21 10:03:46 +0100

    checks: move developer test definition to separate file

--- checks/default.nix
+++ checks/default.nix
@@ -3,19 +3,8 @@
   perSystem =
     { pkgs, ... }:
     {
-      # We mostly check that the module evaluates.
-      checks.developer = pkgs.testers.nixosTest {
-        name = "developer";
-
-        nodes.machine = {
-          imports = [ self.nixosModules.developer ];
-          ctrl-os.developer.enable = true;
-        };
-
-        testScript = ''
-          start_all()
-          machine.wait_for_unit("multi-user.target")
-        '';
+      checks = {
+        developer = pkgs.callPackage ./developer.nix { inherit (self) nixosModules; };
       };
 
     };
