492c4a0708c989ca1632a3b3560cd53deb5abb50
Martin Messer <martin.messer@cyberus-technology.de>
2025-12-11 11:19:55 +0100

    overlays: move overlay definition to packages flakeModule

--- packages/flakeModule.nix
+++ packages/flakeModule.nix
@@ -1,5 +1,18 @@
-{ ... }:
+{ withSystem, ... }:
 {
+  flake.overlays = rec {
+    default = vms;
+    vms =
+      _: prev:
+      withSystem prev.stdenv.hostPlatform.system (
+        { config, ... }:
+        {
+          scl = config.packages.scl;
+          OVMF-cloud-hypervisor = config.packages.OVMF-cloud-hypervisor;
+        }
+      );
+  };
+
   perSystem =
     { pkgs, ... }:
     {
