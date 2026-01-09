faca49c371404c79006b9e72d4e2f79b8064eccc
Martin Messer <martin.messer@cyberus-technology.de>
2026-01-09 13:19:11 +0100

    packages: add packages definition and NVidia Jetson Orin installer

--- packages/flakeModule.nix
+++ packages/flakeModule.nix
@@ -1,4 +1,9 @@
-{ withSystem, inputs, ... }:
+{
+  withSystem,
+  inputs,
+  self,
+  ...
+}:
 { }
 //
   inputs.nixpkgs.lib.optionalAttrs
@@ -18,8 +23,30 @@
       };
 
       perSystem =
-        { pkgs, ... }:
+        { pkgs, system, ... }:
         {
-          packages = import ./default.nix { inherit pkgs; };
+          packages = (import ./default.nix { inherit pkgs; }) // {
+            jetsonOrinNanoInstaller =
+              (inputs.nixpkgs.lib.nixosSystem {
+                modules = [
+                  (
+                    { modulesPath, ... }:
+                    {
+                      imports = [
+                        "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+                        self.nixosModules.platform
+                        self.nixosModules.profiles
+                      ];
+
+                      ctrl-os.profiles.developer.enable = true;
+                      ctrl-os.platform = "nvidia-jetson-orin-nano";
+                      nixpkgs.hostPlatform = "aarch64-linux";
+                      nixpkgs.buildPlatform = system;
+                      system.stateVersion = "25.11";
+                    }
+                  )
+                ];
+              }).config.system.build.isoImage;
+          };
         };
     }
