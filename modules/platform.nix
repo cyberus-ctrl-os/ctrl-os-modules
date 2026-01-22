20b8f177d60b4577f8c0e12fbe9665bb9f31268d
Samuel Dionne-Riel <samuel.dionne-riel@cyberus-technology.de>
2026-01-22 18:46:28 -0500

    modules/platform: Make platform module handle gating off modules
    This will ensure the `mkIf` conditional is always *coherent* with what
    it should be.
    
    Otherwise, copy/paste mistakes can become awkward to debug.
    
    Signed-off-by: Samuel Dionne-Riel <samuel.dionne-riel@cyberus-technology.de>

--- modules/platform.nix
+++ modules/platform.nix
@@ -1,13 +1,13 @@
-{ lib, ... }:
+{ config, lib, ... }@moduleArgs:
 
 let
+  platform = config.ctrl-os.platform;
   inherit
     (import ../lib { inherit lib; })
     getVendorsModules
   ;
   deviceModules = getVendorsModules ./platforms/devices;
   devices = builtins.attrNames deviceModules;
-  deviceDirs = builtins.attrValues deviceModules;
 in
 {
   options.ctrl-os.platform = lib.mkOption {
@@ -16,5 +16,27 @@ in
     default = null;
   };
 
-  imports = deviceDirs;
+  imports = builtins.attrValues (
+    builtins.mapAttrs (
+      device: dir:
+      let
+        # The module may be an attribute set, or a function.
+        module = import dir;
+
+        # Get an attribute set from the module
+        moduleAttrs = if builtins.isAttrs module then module else module moduleArgs;
+
+        # Since we are adding an `mkIf` for the module file,
+        # we need to apply it to the whole `config` value...
+        # ... which may be implicit in a module, so get a `config` in that case.
+        moduleAttrsWithConfig = if moduleAttrs ? config then moduleAttrs else { config = moduleAttrs; };
+      in
+      # Finally, insert the `mkIf` in the module.
+      moduleAttrsWithConfig
+      // {
+        config = lib.mkIf (platform == device) moduleAttrsWithConfig.config;
+      }
+    ) deviceModules
+  );
+
 }
