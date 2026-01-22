912e2b26fce638751b4d0b50e3ee3fcfd76deaf7
Samuel Dionne-Riel <samuel.dionne-riel@cyberus-technology.de>
2026-01-22 12:48:53 -0500

    treewide: Fix jetson-orin-nano-super name
    The device name (even though the vendor does not seem to always spell it
    out), includes “Super”.
    
    I'm using the fact that the innate DeviceTree for the platform uses the
    model name:
    
    ```
    NVIDIA Jetson Orin Nano Engineering Reference Developer Kit Super
    ```
    
    The `compatible` is `nvidia,p3768-0000+p3767-0005-super`.
    
    For this reason, we shall always use “super”.
    
    Signed-off-by: Samuel Dionne-Riel <samuel.dionne-riel@cyberus-technology.de>

--- modules/platform.nix
+++ modules/platform.nix
@@ -4,13 +4,13 @@
     type =
       with lib.types;
       nullOr (enum [
-        "nvidia-jetson-orin-nano"
+        "nvidia-jetson-orin-nano-super"
       ]);
     description = "The platform, we are running on.";
     default = null;
   };
 
   imports = [
-    ./platforms/nvidiaJetsonOrinNano.nix
+    ./platforms/devices/nvidia/jetson-orin-nano-super/default.nix
   ];
 }
