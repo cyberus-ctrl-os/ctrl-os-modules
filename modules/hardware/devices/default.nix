b022c2c325c7d1d6d4773312c7596608d7c43ae9
Samuel Dionne-Riel <samuel.dionne-riel@cyberus-technology.de>
2026-01-23 11:24:36 -0500

    hardware: Reorganize options structure
    This is the final part of a change set that mirrors a commit from
    Julian. This one renames the options from platform to hardware devices,
    better future-proofing the options.
    
    This also has the benefit of not re-using a term (platform) that has an
    existing meaning with Nixpkgs things (`stdenv.hostPlatform.system`), and
    should make it less confusing to discuss about.
    
    Co-authored-by: Julian Stecklina <julian.stecklina@cyberus-technology.de>
    Signed-off-by: Samuel Dionne-Riel <samuel.dionne-riel@cyberus-technology.de>

--- modules/hardware/devices/default.nix
+++ modules/hardware/devices/default.nix
@@ -1,50 +1,41 @@
 { config, lib, ... }@moduleArgs:
 
 let
-  platform = config.ctrl-os.platform;
-  inherit
-    (import ../../../lib { inherit lib; })
+  cfg = config.ctrl-os.hardware;
+  inherit (import ../../../lib { inherit lib; })
     getVendorsModules
     ;
   deviceModules = getVendorsModules ./.;
   devices = builtins.attrNames deviceModules;
 in
 {
-  options.ctrl-os.platform = lib.mkOption {
+  options.ctrl-os.hardware.device = lib.mkOption {
     type = with lib.types; nullOr (enum devices);
-    description = "The platform, we are running on.";
+    description = "Selects a hardware device profile to use by device name.";
     default = null;
   };
 
-  imports =
-    builtins.attrValues
-    (
-      builtins.mapAttrs
-      (
-        device: dir:
-        let
-          cfgOrFn = import dir;
-          appliedConfig =
-            let config = cfgOrFn moduleArgs; in
-            if config ? config then
-              config
-            else
-              { inherit config; }
-          ;
-          cond = lib.mkIf (platform == device);
-        in
-          if builtins.isAttrs cfgOrFn then
-            {
-              config = cond (
-                cfgOrFn
-              );
-            }
-          else
-            appliedConfig // {
-              config = cond appliedConfig.config;
-            }
-      )
-      deviceModules
-    )
-  ;
+  imports = builtins.attrValues (
+    builtins.mapAttrs (
+      device: dir:
+      let
+        cfgOrFn = import dir;
+        appliedConfig =
+          let
+            config = cfgOrFn moduleArgs;
+          in
+          if config ? config then config else { inherit config; };
+        cond = lib.mkIf (cfg.device == device);
+      in
+      if builtins.isAttrs cfgOrFn then
+        {
+          config = cond (cfgOrFn);
+        }
+      else
+        appliedConfig
+        // {
+          config = cond appliedConfig.config;
+        }
+    ) deviceModules
+  );
 }
